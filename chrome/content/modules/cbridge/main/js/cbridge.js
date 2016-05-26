c709b948eab23f4d2d63febc13cbec697d33f6f0
ClonkGeist <clonkgeist@cmc.re>
2016-05-26 15:09:39 +0200

    * Deutsches Sprachpaket aktualisiert

--- chrome/content/modules/cbridge/main/js/cbridge.js
+++ chrome/content/modules/cbridge/main/js/cbridge.js
@@ -6,10 +6,10 @@ window.addEventListener("load", function(){
 
 	var deckitems = [deck.add(getModule(modules[0], true), 0),
 					 deck.add(getModule(modules[1], true), 0, false, false, true)];
-	_mainwindow.addNavigationItem("Network games", "cbridge", true, 0, function() {
+	_mainwindow.addNavigationItem(Locale("$NavNetworkGames$"), "cbridge", true, 0, function() {
 		togglePage(deck.id, deckitems[0]);
 	});
-	_mainwindow.addNavigationItem("Host game", "cbridge", 0, 0, function() {
+	_mainwindow.addNavigationItem(Locale("$NavHostGame$"), "cbridge", 0, 0, function() {
 		togglePage(deck.id, deckitems[1]);
 	});
 });
